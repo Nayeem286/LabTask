32ff57f5b012a645342c2397a27f5dabce48d150
Nafiul Nayeem <125290127+Nayeem286@users.noreply.github.com>
2023-02-13 19:29:40 +0300

    Lab Task

---
+++ Login.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+ <html> 
+ <head> 
+ <title› Login Page‹/title>
+ </head>
+ <body>
+  <hl>Login</h1>
+  </fieldset>
+
+<fieldset style="width: 300px; padding: 05px;">
+<legend>Login</legend>
+<label for="Username :" > <b> Username :</b> </label> 
+<input type= "text" id="Username" name="Username"> <br><br>
+<label for= "Password :"> <b>Password:</b></label>
+ <input type="text" id="Password" name= "Password"><br><br>
+<input type="submit" value="Login"> 
+</fieldset>
+
+<p> Don't have an account? Register <a href=file:///C:/Web%20developement/project/registration.html>here</a> </p>
+</form>
+</body>
+</html>
+
